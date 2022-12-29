8b68bd2ee664d951d165118a09cda2585a3eff38
calimad <calimanescuadrian@gmail.com>
2022-12-29 17:39:35 +0200

    Create the data frame and check it with print

--- definition.py
+++ definition.py
@@ -1,7 +1,16 @@
-class Definition:
+import pandas
 
+
+class Definition:
+    """The Data Frame contains two colums, word and definition"""
     def __init__(self, term):
         self.term = term
 
     def get(self):
-        pass
+        df = pandas.read_csv("data.csv")
+        return tuple(df.loc[df["word"]==self.term]["definition"])
+
+d = Definition(term="sun")
+print(d.get())
+
+
