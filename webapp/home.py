8b55e03605e8b47148dd113da4f4bd5bff9d9875
calimad <calimanescuadrian@gmail.com>
2023-01-02 15:31:51 +0200

    Layout with Menu

--- webapp/home.py
+++ webapp/home.py
@@ -1,10 +1,46 @@
 import justpy as jp
 
+"""Visit https://quasar.dev and https://tailwindcss.com to see all the info about styling.
+a=div means thats the grid will be on top of div which has a=wp main gray background.
+a=div1 means that all of the objects are placed on div1 which is a=div which is a=wp. 
+No comma for tailwind, just use space with classes=""
+For Quasar put QDiv.
+Classes only change the styling. Actions are controled by functions, eg: Button. 
+! click=sum_up without () !
+Click and mouseenter are the same thing!
+Quasar is more advanced but supports Tailwind. Just write tailwind=True
+"""
+
 class Home:
-    path = "/"
-    def serve(self):
+    path = "/home"
+
+    @classmethod
+    def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
-        div = jp.Div(a=wp, classes="bg-gray-300 h-screen")
+
+        layout = jp.QLayout(a=wp, view="hHh lpR fFf")
+        header = jp.QHeader(a=layout)
+        toolbar = jp.QToolbar(a=header)
+
+        drawer = jp.QDrawer(a=layout, show_if_above=True, v_mode="left",
+                            bordered=True)
+        a_classes = "p-2 m-2 test-lg text-blue-400 hover:text-blue-700"
+        scroller = jp.QScrollArea(a=drawer, classes="fit")
+        qlist = jp.QList(a=scroller)
+        jp.A(a=qlist, text="Home", href="/home", classes=a_classes)
+        jp.Br(a=qlist)
+        jp.A(a=qlist, text="About", href="/about", classes=a_classes)
+        jp.Br(a=qlist)
+        jp.A(a=qlist, text="Dictionary", href="/dictionary", classes=a_classes)
+        jp.Br(a=qlist)
+
+        jp.QBtn(a=toolbar, dense=True, flat=True, round=True, icon="menu",
+                click=cls.move_drawer, drawer=drawer)
+        jp.QToolbarTitle(a=toolbar, text="Instant Dictionary")  # so that the button is in front of text
+
+        container = jp.QPageContainer(a=layout)
+
+        div = jp.Div(a=container, classes="bg-gray-300 h-screen p-2")
         jp.Div(a=div, text="This is the Home page", classes="text-4xl m-2")
         jp.Div(a=div, text="""Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
@@ -16,4 +52,9 @@ class Home:
                 anim id est laborum.""", classes="text-lg")
         return wp
 
-
+    @staticmethod
+    def move_drawer(widget, msg):
+        if widget.drawer.value is True:
+            widget.drawer.value = False
+        else:
+            widget.drawer.value = True
